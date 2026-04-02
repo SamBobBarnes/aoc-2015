986505646471a9480595ba320278a36d5fa27e0b
SamBobBarnes <sbarnes@vermeer.com>
2026-04-02 10:43:17 -0500

    day 1

--- day1.c
+++ day1.c
@@ -4,16 +4,38 @@
 
 #include "day1.h"
 #include "Inputs/day1.h"
+#include <string.h>
 
 #include <stdio.h>
 
 void day1_part1() {
     print_header(1,1);
-    char* input = day1_input.test_input;
+    char* input = day1_input.input;
+    size_t len = strlen(input);
+    int floor = 0;
+    for (int i = 0; i < len; ++i) {
+        if (input[i] == ')') floor--;
+        else floor++;
+    }
+
+    printf("Santa should go to floor %i", floor);
 }
 
 void day1_part2() {
     print_header(1,2);
+    char* input = day1_input.input;
+    size_t len = strlen(input);
+    int floor = 0;
+    for (int i = 0; i < len; ++i) {
+        if (input[i] == ')') floor--;
+        else floor++;
+        if (floor == -1) {
+
+            printf("Santa entered the basement on char %i", i+1);
+            break;
+        }
+    }
+
 }
 
 IDay day1 = {
