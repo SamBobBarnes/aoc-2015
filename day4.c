2171d03394dd5f626c7d315911e086d8163195e4
SamBobBarnes <sbarnes@vermeer.com>
2026-04-09 09:51:42 -0500

    d4p2

--- day4.c
+++ day4.c
@@ -60,7 +60,42 @@ void day4_part1() {
 
 void day4_part2() {
     print_header(4, 2);
-    char *input = day4_input.test_input;
+    const char *input = day4_input.input;
+
+    int seed_num = 0;
+    char seed[20];
+    uint8_t final_result[16] = {0};
+
+    sprintf(seed, "%s%d", input, seed_num);
+
+    do {
+        uint8_t result[16] = {0};
+        md5_context ctx;
+        md5_init(&ctx);
+        md5_digest(&ctx, seed, strlen(seed));
+        md5_output(&ctx, result);
+
+        // if (seed_num % 1000 == 0 ) {
+        // if (seed_num == 609043) {
+        //     print_bytes(result);
+        //     printf("seed_num: %i\nseed: %s\n------\n", seed_num, seed);
+        // }
+
+        if (
+            result[0] == 0
+            && result[1] == 0
+            && result[2] == 0
+            && result[3] > 0x0f
+        ) {
+            memcpy(final_result, result, 16);
+            break;
+        }
+        seed_num++;
+        sprintf(seed, "%s%d", input, seed_num);
+    } while (true);
+
+    print_bytes(final_result);
+    printf("seed_num: %i\nseed: %s\n", seed_num, seed);
 }
 
 
