ba771a02a927a6045c156828597da50ee42aaf32
SamBobBarnes <sbarnes@vermeer.com>
2026-04-13 16:11:00 -0500

    d10p1

--- day10.c
+++ day10.c
@@ -7,10 +7,46 @@
 
 #include <string.h>
 
+#include "debug.h"
+
 void day10_part1() {
     print_header(10, 1);
-    const char *input = day10_input.test_input;
-    const size_t len = strlen(input);
+    const char *input = day10_input.input;
+
+    char prev[1000000] = {'\0'};
+    char next[1000000] = {'\0'};
+    strcpy(prev, input);
+
+    int reps = 40;
+    for (int r = 0; r < reps; r++) {
+        size_t len = strlen(prev);
+        debug("%llu\n", len);
+        int run_len = 1;
+        char run_char = prev[0];
+
+        for (int i = 1; i < len; i++) {
+            if (prev[i] == run_char) run_len++;
+            else {
+                char run_len_string[5] = {'\0'};
+                itoa(run_len, run_len_string, 10);
+                strcat(next, run_len_string);
+                char run_char_string[2] = {run_char, '\0'};
+                strcat(next, run_char_string);
+                run_len = 1;
+                run_char = prev[i];
+            }
+        }
+
+        char run_len_string[5] = {'\0'};
+        itoa(run_len, run_len_string, 10);
+        strcat(next, run_len_string);
+        char run_char_string[2] = {run_char, '\0'};
+        strcat(next, run_char_string);
+        strcpy(prev, next);
+        for (int i = 0; i < strlen(next); i++) next[i] = '\0';
+    }
+
+    printf("%llu", strlen(prev));
 }
 
 void day10_part2() {
