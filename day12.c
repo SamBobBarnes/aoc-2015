42083cc0289c04403b3242970d39474b527c8fa5
SamBobBarnes <sbarnes@vermeer.com>
2026-04-14 13:18:17 -0500

    wip d12

--- day12.c
+++ day12.c
@@ -7,6 +7,8 @@
 
 #include <string.h>
 
+#include "debug.h"
+
 void day12_part1() {
     print_header(12, 1);
     bool test = false;
@@ -39,7 +41,7 @@ void day12_part1() {
         }
     }
 
-    printf("The sum of all number is %i", total);
+    printf("The sum of all numbers is %i", total);
 
     if (!test) {
         input = input - 3;
@@ -49,7 +51,7 @@ void day12_part1() {
 
 void day12_part2() {
     print_header(12, 2);
-    bool test = true;
+    bool test = false;
     char *input = nullptr;
     if (test)
         input = day12_input.test_input;
@@ -58,6 +60,59 @@ void day12_part2() {
         input = input + 3;
     }
     const size_t len = strlen(input);
+
+    bool reading_num = false;
+    char num[10] = {'\0'};
+    int num_i = 0;
+    int total = 0;
+    int depth = 0;
+    int reds = 0;
+    int depth_total = 0;
+    int stop_reading = -1;
+    for (int i = 0; i < len; i++) {
+        char val = input[i];
+        if (stop_reading == -1) {
+            if ((input[i] >= 48 && input[i] <= 57) || input[i] == '-') reading_num = true;
+            else if (reading_num) {
+                int num_val = atoi(num);
+                depth_total += num_val;
+                reading_num = false;
+                for (int j = 0; j < 10; j++) {
+                    num[j] = '\0';
+                }
+                num_i = 0;
+            }
+            if (reading_num) {
+                num[num_i++] = input[i];
+            }
+        }
+        if (input[i] == '{' || input[i] == '[') {
+            if (stop_reading == -1) {
+                total += depth_total;
+            }
+            depth++;
+            depth_total = 0;
+        }
+        if (input[i] == '}' || input[i] == ']') {
+            if (depth == stop_reading - 1) {
+                stop_reading = -1;
+                depth_total = 0;
+            }
+            total += depth_total;
+            depth--;
+            depth_total = 0;
+        }
+        if (i < len - 6 && input[i] == ':' && input[i + 1] == '"' && input[i + 2] == 'r' && input[i + 3] == 'e' && input
+            [i + 4] == 'd' && input[i + 5] == '"') {
+            reds++;
+            stop_reading = depth;
+            depth_total = 0;
+        }
+    }
+
+    debug_ln("reds: %i", reds);
+    // 82040 < x < 121920
+    printf("The sum of all numbers is %i", total);
 
     if (!test) {
         input = input - 3;
