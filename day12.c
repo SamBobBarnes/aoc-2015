4169ac7639cc22da73b602105535e1725156496f
SamBobBarnes <sbarnes@vermeer.com>
2026-04-14 15:28:39 -0500

    wip day 12p2

--- day12.c
+++ day12.c
@@ -49,6 +49,60 @@ void day12_part1() {
     }
 }
 
+int reds = 0;
+
+int recurse(int *i, const char *json, const size_t len, const int depth) {
+    int total = 0;
+    bool red = false;
+    char container = *i != 0 ? json[(*i) - 1] : 'r';
+
+    bool reading_num = false;
+    char num[10] = {'\0'};
+    int num_i = 0;
+    while (*i < len) {
+        const char val = json[*i];
+        if (val == '{' || val == '[') {
+            (*i)++;
+            total += recurse(i, json, len, depth + 1);
+        }
+        if (val == '}' || val == ']') {
+            (*i)++;
+            break;
+        }
+        if (*i < len - 3 && json[*i] == 'r' && json[*i + 1] == 'e' && json[*i + 2] == 'd' && container == '{') {
+            red = true;
+        }
+        if ((val >= 48 && val <= 57) || val == '-') reading_num = true;
+        else if (reading_num) {
+            int num_val = atoi(num);
+            total += num_val;
+            reading_num = false;
+            for (int j = 0; j < 10; j++) {
+                num[j] = '\0';
+            }
+            num_i = 0;
+        }
+        if (reading_num) {
+            num[num_i++] = val;
+        }
+
+        (*i)++;
+    }
+
+    char spacer[200] = {'\0'};
+    for (int j = 0; j < depth; j++)
+        spacer[j] = ' ';
+
+    if (red) {
+        debug_ln(RED "%s%c d: %i, t: %i" RESET, spacer, container, depth, total, red);
+        reds++;
+        return 0;
+    }
+    debug_ln("%s%c d: %i, t: %i", spacer, container, depth, total, red);
+
+    return total;
+}
+
 void day12_part2() {
     print_header(12, 2);
     bool test = false;
@@ -61,58 +115,12 @@ void day12_part2() {
     }
     const size_t len = strlen(input);
 
-    bool reading_num = false;
-    char num[10] = {'\0'};
-    int num_i = 0;
-    int total = 0;
-    int depth = 0;
-    int reds = 0;
-    int depth_total = 0;
-    int stop_reading = -1;
-    for (int i = 0; i < len; i++) {
-        char val = input[i];
-        if (stop_reading == -1) {
-            if ((input[i] >= 48 && input[i] <= 57) || input[i] == '-') reading_num = true;
-            else if (reading_num) {
-                int num_val = atoi(num);
-                depth_total += num_val;
-                reading_num = false;
-                for (int j = 0; j < 10; j++) {
-                    num[j] = '\0';
-                }
-                num_i = 0;
-            }
-            if (reading_num) {
-                num[num_i++] = input[i];
-            }
-        }
-        if (input[i] == '{' || input[i] == '[') {
-            if (stop_reading == -1) {
-                total += depth_total;
-            }
-            depth++;
-            depth_total = 0;
-        }
-        if (input[i] == '}' || input[i] == ']') {
-            if (depth == stop_reading - 1) {
-                stop_reading = -1;
-                depth_total = 0;
-            }
-            total += depth_total;
-            depth--;
-            depth_total = 0;
-        }
-        if (i < len - 6 && input[i] == ':' && input[i + 1] == '"' && input[i + 2] == 'r' && input[i + 3] == 'e' && input
-            [i + 4] == 'd' && input[i + 5] == '"') {
-            reds++;
-            stop_reading = depth;
-            depth_total = 0;
-        }
-    }
+    int i = 0;
+    const int total = recurse(&i, input, len, 0);
 
-    debug_ln("reds: %i", reds);
     // 82040 < x < 121920
-    printf("The sum of all numbers is %i", total);
+    print_ln("reds: %i", reds);
+    print_ln("The sum of all numbers is %i", total);
 
     if (!test) {
         input = input - 3;
