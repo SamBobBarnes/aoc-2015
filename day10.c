db35a0d649c9ac6bb3562b1282a14d3b6183b28b
SamBobBarnes <sbarnes@vermeer.com>
2026-04-14 11:01:00 -0500

    d10p2

--- day10.c
+++ day10.c
@@ -5,16 +5,17 @@
 #include "day10.h"
 #include "Inputs/day10.h"
 
+#include <stdint.h>
 #include <string.h>
 
 #include "debug.h"
 
 typedef struct {
-    int count;
+    uint8_t count;
     char number;
 } Run;
 
-#define RUN_ARRAY_LEN 100000
+#define RUN_ARRAY_LEN 5000000
 
 void print_run(const Run *r) {
     printf("(c: %i, n: %c, s: '", r->count, r->number);
@@ -24,7 +25,7 @@ void print_run(const Run *r) {
     printf("')\n");
 }
 
-void decode_run(const Run *r, char string[2]) {
+void decode_run(const Run *r, char string[3]) {
     sprintf(string, "%d%c", r->count, r->number);
 }
 
@@ -81,7 +82,8 @@ int sum_runs(const Run *prev, const int prev_len) {
 }
 
 size_t calculate(const char *input, const int reps) {
-    Run prev[RUN_ARRAY_LEN] = {1, input[0]};
+    Run *prev = malloc(RUN_ARRAY_LEN * 2);
+    prev[0] = (Run){1, input[0]};
     int prev_len = 1;
     int prev_i = 0;
 
@@ -98,15 +100,17 @@ size_t calculate(const char *input, const int reps) {
         for (int i = 0; i < prev_len; i++)
             print_run(&prev[i]);
 
-    Run next[RUN_ARRAY_LEN] = {0, '0'};
+    Run *next = malloc(RUN_ARRAY_LEN * 2);
+    next[0] = (Run){0, '0'};
     int next_len = 0;
     int next_i = 0;
 
     for (int r = 0; r < reps; r++) {
         next_i = 0;
+        debug_ln("%i: %i", r, prev_len);
         for (int i = 0; i < prev_len; i++) {
-            char string[2];
-            decode_run(&prev[i], &string[0]);
+            char string[3];
+            decode_run(&prev[i], string);
             const int encoded_count = encode_run(&next[next_i++], string);
             if (encoded_count > 1) next_i++;
         }
@@ -123,7 +127,10 @@ size_t calculate(const char *input, const int reps) {
         // }
     }
 
-    return sum_runs(prev, prev_len);
+    const int sum = sum_runs(prev, prev_len);
+    free(prev);
+    free(next);
+    return sum;
 }
 
 void day10_part1() {
