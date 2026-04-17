e63fe0509e20fef4d888e8a6324341dfa437b8df
SamBobBarnes <sbarnes@vermeer.com>
2026-04-17 13:14:50 -0500

    wip d17

--- day17.c
+++ day17.c
@@ -42,12 +42,15 @@ bool contains(const Box *boxes, const int box_count, const Box *box) {
 void recurse_boxes(const Box *boxes, const int box_count, Box boxes_used[box_count], const int box_used_count,
                    Box *combinations[][box_count], int *combination_counts, int *total, const int goal) {
     int total_volume = total_vol(boxes_used, box_count);
-    if (total_volume > goal) return;
+    if (total_volume > goal) return; // if goal exceeded return
     if (total_volume == goal) {
+        // if goal met
         for (int i = 0; i < *total; i++) {
+            // if an identical set is found, dont double count return
             if (identical_arrays((Box *) combinations[i], combination_counts[i], boxes_used, box_used_count)) return;
         }
         for (int i = 0; i < box_used_count; i++) {
+            // finally, write to combinations and add to total
             *combinations[*total][i] = boxes_used[i];
         }
         combination_counts[*total] = box_used_count;
@@ -55,28 +58,43 @@ void recurse_boxes(const Box *boxes, const int box_count, Box boxes_used[box_cou
     }
 
     for (int i = 0; i < box_count; i++) {
-        if (contains(boxes_used, box_used_count, &boxes[i])) continue;
+        if (contains(boxes_used, box_used_count, &boxes[i])) continue; // if box already in set, continue to the next
         boxes_used[box_used_count] = boxes[i];
-        recurse_boxes(boxes, box_count, boxes_used, box_used_count + 1, combinations, combination_counts, total, goal);
+        recurse_boxes(boxes, box_count, boxes_used, box_used_count + 1, combinations, combination_counts, total,
+                      goal); // recurse
     }
 }
 
 void day17_part1() {
     print_header(17, 1);
     char *input = day17_input.test_input;
+    size_t len = strlen(input);
+
+    char line[len + 1];
+    strcpy(line, input);
 
     Box boxes[20];
     int box_count = 0;
 
-    // int volume = 150;
-    int volume = 25;
+    // int goal = 150;
+    int goal = 25;
 
-    char *ptr = strtok(input, ",");
+    char *ptr = strtok(line, ",");
     while (ptr != NULL) {
         boxes[box_count].id = box_count;
         boxes[box_count++].volume = atoi(ptr);
         ptr = strtok(nullptr, ",");
     }
+
+    Box *combinations[20 * 20][box_count];
+    int combination_counts[20 * 20];
+    Box boxes_used[20];
+    int box_used_count = 0;
+
+    int total;
+
+    recurse_boxes(boxes, box_count, boxes_used, box_used_count, combinations, combination_counts, &total, goal);
+    print_ln("%i", total);
 }
 
 void day17_part2() {
