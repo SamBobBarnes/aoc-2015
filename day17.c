cda10af090a6173ac0fdbe2fe7f4819273fbf650
SamBobBarnes <sbarnes@vermeer.com>
2026-04-17 15:54:39 -0500

    wip

--- day17.c
+++ day17.c
@@ -12,54 +12,57 @@ typedef struct {
     int volume;
 } Box;
 
-int total_vol(Box *boxes, int box_count) {
+int total_vol(Box **boxes, const int box_count) {
     int total = 0;
     for (int i = 0; i < box_count; i++) {
-        total += boxes[i].volume;
+        total += boxes[i]->volume;
     }
     return total;
 }
 
-bool identical_arrays(const Box *boxes_a, const int boxes_a_count, const Box *boxes_b, const int boxes_b_count) {
+bool identical_arrays(const int **boxes_a, const int boxes_a_count, const int **boxes_b, const int boxes_b_count) {
     if (boxes_a_count != boxes_b_count) return false;
     for (int i = 0; i < boxes_a_count; i++) {
         bool match = false;
         for (int j = 0; j < boxes_a_count; j++) {
-            if (boxes_a[i].id == boxes_b[j].id) match = true;
+            if (*boxes_a[i] == *boxes_b[j]) match = true;
         }
         if (!match) return false;
     }
     return true;
 }
 
-bool contains(const Box *boxes, const int box_count, const Box *box) {
+bool contains(const Box **boxes, const int box_count, const Box *box) {
     for (int i = 0; i < box_count; i++) {
-        if (boxes[i].id == box->id) return true;
+        if (boxes[i]->id == box->id) return true;
     }
     return false;
 }
 
-void recurse_boxes(const Box *boxes, const int box_count, Box boxes_used[box_count], const int box_used_count,
-                   Box *combinations[][box_count], int *combination_counts, int *total, const int goal) {
-    int total_volume = total_vol(boxes_used, box_count);
+void recurse_boxes(const Box **boxes, const int box_count, Box boxes_used[box_count], const int box_used_count,
+                   int ***combinations, int **combination_counts, int *total, const int goal) {
+    int total_volume = total_vol(&boxes_used, box_count);
     if (total_volume > goal) return; // if goal exceeded return
     if (total_volume == goal) {
         // if goal met
         for (int i = 0; i < *total; i++) {
             // if an identical set is found, dont double count return
-            if (identical_arrays((Box *) combinations[i], combination_counts[i], boxes_used, box_used_count)) return;
+            int boxids_used[box_used_count];
+            for (int j = 0; j < box_used_count; j++) boxids_used[j] = boxes_used[j].id;
+            if (identical_arrays(combinations[i], *combination_counts[i], (int **) &boxids_used, box_used_count))
+                return;
         }
         for (int i = 0; i < box_used_count; i++) {
             // finally, write to combinations and add to total
-            *combinations[*total][i] = boxes_used[i];
+            *combinations[*total][i] = boxes_used[i].id;
         }
-        combination_counts[*total] = box_used_count;
+        *combination_counts[*total] = box_used_count;
         (*total)++;
     }
 
     for (int i = 0; i < box_count; i++) {
-        if (contains(boxes_used, box_used_count, &boxes[i])) continue; // if box already in set, continue to the next
-        boxes_used[box_used_count] = boxes[i];
+        if (contains(&boxes_used, box_used_count, boxes[i])) continue; // if box already in set, continue to the next
+        boxes_used[box_used_count] = *boxes[i];
         recurse_boxes(boxes, box_count, boxes_used, box_used_count + 1, combinations, combination_counts, total,
                       goal); // recurse
     }
@@ -86,14 +89,15 @@ void day17_part1() {
         ptr = strtok(nullptr, ",");
     }
 
-    Box *combinations[20 * 20][box_count];
+    int *combinations[20 * 20][box_count];
     int combination_counts[20 * 20];
     Box boxes_used[20];
     int box_used_count = 0;
 
     int total;
 
-    recurse_boxes(boxes, box_count, boxes_used, box_used_count, combinations, combination_counts, &total, goal);
+    recurse_boxes((Box **) &boxes, box_count, boxes_used, box_used_count, (int ***) combinations,
+                  (int **) &combination_counts, &total, goal);
     print_ln("%i", total);
 }
 
