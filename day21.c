279df5bdb965a23090c544a0e824da657d655d24
SamBobBarnes <sbarnes@vermeer.com>
2026-05-11 14:05:07 -0500

    wip

--- day21.c
+++ day21.c
@@ -7,16 +7,40 @@
 
 #include <string.h>
 
+typedef struct {
+    int hp;
+    int damage;
+    int armor;
+} Stats;
+
 void day21_part1() {
     print_header(21, 1);
-    const char *input = day21_input.test_input;
-    const size_t len = strlen(input);
+    bool test = true;
+
+    Stats player;
+    Stats opponent;
+    if (test) {
+        player = (Stats){8, 5, 5};
+        opponent = (Stats){12, 7, 2};
+    } else {
+        player = (Stats){100, 0, 0};
+        opponent = (Stats){104, 8, 1};
+    }
 }
 
 void day21_part2() {
     print_header(21, 2);
-    const char *input = day21_input.test_input;
-    const size_t len = strlen(input);
+    bool test = true;
+
+    Stats player;
+    Stats opponent;
+    if (test) {
+        player = (Stats){8, 5, 5};
+        opponent = (Stats){12, 7, 2};
+    } else {
+        player = (Stats){100, 0, 0};
+        opponent = (Stats){104, 8, 1};
+    }
 }
 
 IDay day21 = {
