7fc3ef26ea3b51c4a4137ecc973293225e1ca59f
SamBobBarnes <sbarnes@vermeer.com>
2026-05-14 09:26:07 -0500

    wip d25

--- day25.c
+++ day25.c
@@ -5,12 +5,37 @@
 #include "day25.h"
 #include "Inputs/day25.h"
 
+#include <stdint.h>
 #include <string.h>
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+uint64_t calculate_next(const uint64_t last) {
+    uint64_t mul = (uint64_t) 252533 * last;
+    return mul % 33554393;
+}
+
+Point next_point(const Point current) {
+    if (current.y == 1) return (Point){1, current.x + 1};
+    return (Point){current.x + 1, current.y - 1};
+}
+
 
 void day25_part1() {
     print_header(25, 1);
-    const char *input = day25_input.test_input;
-    const size_t len = strlen(input);
+    Point goal = {3019, 3010};
+
+    uint64_t current_val = 20151125;
+    Point current = {1, 1};
+    while (current.x != goal.x && current.y != goal.y) {
+        current = next_point(current);
+        current_val = calculate_next(current_val);
+    }
+    // 6483627 < x < 22512812
+    print_ln("code is %lld", current_val);
 }
 
 void day25_part2() {
